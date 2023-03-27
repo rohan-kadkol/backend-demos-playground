dc8a032b3357b094bdd9cdaf9cd4d7e6d42ca24b
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-03-27 06:02:25 -0400

    Fast api trying out streaming responses

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,9 @@
 from typing import Optional, Union
 
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+import time
 
 app = FastAPI()
 
@@ -14,6 +17,18 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/items/{item_id}")
 def add_item(item_id: int, q: Optional[str] = None):
     return f"Item with id{item_id} is added 😊"
+
+
+async def fake_video_streamer():
+    for i in range(10):
+        time.sleep(0.5)
+        yield b"some fake video bytes"
+
+
+@app.get("/video")
+async def fake_video_streamer_endpoint():
+    return StreamingResponse(fake_video_streamer())
